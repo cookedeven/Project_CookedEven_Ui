040051d719418840e76b9977545d121118a5cb98
harimi1 <bsy200100@gmail.com>
2025-06-14 18:45:18 +0900

    v0.2.0
    5페이지 추가(스크롤 미완)

---
+++ Assets/Sprites/MapItem.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapItem : MonoBehaviour
+{
+    private Map _mapData;
+
+    [SerializeField]
+    private Image _mapPic;
+    [SerializeField]
+    private Text _mapName;
+
+    public void SetMapItem(Map )
+}
