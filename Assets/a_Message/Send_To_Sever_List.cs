f6503967fa8224b848c0c6f01319cc17dc8e4d27
harimi1 <bsy200100@gmail.com>
2025-07-18 19:53:49 +0900

    v0.8.0
    UI 바꿈, 클라 to 서버, 서버 to 클라 전송 리스트 추가

---
+++ Assets/a_Message/Send_To_Sever_List.cs
@@ -0,0 +1,98 @@
+/*
+ * Scene 1 --------------------- Local
+ * 
+ * Client To Server
+ *  None
+ * 
+ * Server To Client
+ *  None
+ * 
+ * Scene 2 ---------------------
+ * 
+ * Client To Server
+ *  1. Player_Name
+ * 
+ * Server To Client
+ *  1. All Player_Name (DB)
+ *  
+ * Scene 3 --------------------- Local
+ * 
+ * Client To Server
+ *  None
+ * 
+ * Server To Client
+ *  None
+ *
+ * Scene 4 --------------------- 
+ * 
+ * Client To Server
+ *  1. Selected_Mode
+ * 
+ * Server To Client
+ *  None
+ *
+ * Scene 5 --------------------- 
+ * 
+ * Client To Server
+ *  1. Selected_Character
+ *  2. Selected_Map
+ * 
+ * Server To Client
+ *  None
+ *  
+ * Scene 6 --------------------- 
+ * 
+ * Client To Server
+ *  1. Selected_Map
+ *  2. Player_Data
+ * 
+ * Server To Client
+ *  1. MatchingStatus
+ *  2. MatchingTime
+ *  3. IsMatched
+ *  4. Server_Status
+ *  5. Room_Ping
+ *  6. Room_Packet_Loss_Percentage
+ *
+ * Scene 7 --------------------- 
+ * 
+ * Client To Server
+ *  1. Player_Data
+ *  2. IsPlayerLoaded (각 플레이어가 로딩이 끝났는지)
+ * 
+ * Server To Client
+ *  1. IsPlayerLoaded
+ *  
+ * Scene 8 --------------------- 
+ * 
+ * Client To Server
+ *  1. Player_Data
+ * 
+ * Server To Client
+ *  1. Time (남은 시간)
+ *  2. Blue_Menu1_Ingredients
+ *  2-1. Blue_Menu1_Time
+ *  3. Blue_Menu2_Ingredients
+ *  3-1. Blue_Menu2_Time
+ *  4. Blue_Menu3_Ingredients
+ *  4-1. Blue_Menu3_Time
+ *  5. Blue_Score
+ *  6. Red_Menu1_Ingredients
+ *  6-1. Red_Menu1_Time
+ *  7. Red_Menu2_Ingredients
+ *  7-1. Red_Menu2_Time
+ *  8. Red_Menu3_Ingredients
+ *  8-1. Red_Menu3_Time
+ *  9. Red_Score
+ *  10. Player_Data
+ *  
+ * Scene 9 --------------------- Local
+ * 
+ * Client To Server
+ *  None
+ * 
+ * Server To Client
+ *  None
+ *  
+ *
+ */
